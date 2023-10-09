3330761c4104dbdee83c00e72591337958f24e84
andrey.malygin2002@gmail.com <Serega@1056>
2023-10-09 10:06:22 +0300

    init commit Kafka starter test

---
+++ kafka-transaction/src/test/java/com/example/kafkatransaction/KafkaTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkatransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTransactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
